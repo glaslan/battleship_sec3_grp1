f6e31c001d90c124fc5ea398e6eb7696a2e6f99c
Andrew Abrantes <145807329+AndrewAConestoga@users.noreply.github.com>
2025-03-08 16:53:43 -0500

    Added basic functions for board, tiles and a constants class

---
+++ Client/src/main/java/com/belgianwaffles/battleship/Constants.java
@@ -0,0 +1,9 @@
+package com.belgianwaffles.battleship;
+
+public class Constants {
+
+    public static final int PLAYER1 = 0;
+    public static final int PLAYER2 = 1;
+    public static final int BOARD_DIMENSIONS = 8;
+    
+}
