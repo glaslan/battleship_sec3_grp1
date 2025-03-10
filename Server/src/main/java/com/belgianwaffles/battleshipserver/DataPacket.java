625772dfac04ef0a149549a2ff61220a2bb8e59e
Schtuffs <152662980+Schtuffs@users.noreply.github.com>
2025-03-10 15:56:46 -0400

    Some changed to packet design and started connection abstraction

--- Server/src/main/java/com/belgianwaffles/battleshipserver/DataPacket.java
+++ Server/src/main/java/com/belgianwaffles/battleshipserver/DataPacket.java
@@ -1,5 +1,141 @@
 package com.belgianwaffles.battleshipserver;
 
+public final class DataPacket {
+
+    // ----- Subclasses -----
+    
+    public final class Header {
+        
+        // ----- Constants -----
+        
+        public static final int HEADER_SIZE       = 5;
+
+        public static final byte HEAD_TYPE_PING   = 1;
+        public static final byte HEAD_TYPE_GAME   = 2;
+
+        private final byte HEAD_MASK_FLAGS   = (byte)0b11110000;
+        private final byte HEAD_MASK_TYPE    = (byte)0b00001110;
+        private final byte HEAD_MASK_TURN    = (byte)0b00000001;
+        private final byte HEAD_MASK_USER    = (byte)0b11111111;
+        private final byte HEAD_MASK_LENGTH  = (byte)0b11111111;
+
+        private final int HEAD_INDEX_FLAGS  = 0;
+        private final int HEAD_INDEX_TYPE   = 0;
+        private final int HEAD_INDEX_TURN   = 0;
+        private final int HEAD_INDEX_USER   = 1;
+        private final int HEAD_INDEX_LENGTH = 3;
+
+        // ----- Data -----
+        
+        // Contains all header data after packing
+        private byte[] mData;
+
+        // ----- Methods -----
+
+        public Header() {
+            this.mData = new byte[HEADER_SIZE];
+        }
+
+        // ----- Bit ----- Manipulators -----
+
+        private void bitManipulate(int byteNum, byte mask, byte newData) {
+            this.mData[byteNum] |= mask & newData;
+        }
+
+        public void addFlag(byte flag) {
+            this.bitManipulate(HEAD_INDEX_FLAGS, HEAD_MASK_FLAGS, flag);
+        }
+        
+        public void addType(byte type) {
+            this.bitManipulate(HEAD_INDEX_TYPE, HEAD_MASK_TYPE, (byte)(type << 1));
+        }
+        
+        public void addTurn(byte turn) {
+            this.bitManipulate(HEAD_INDEX_TURN, HEAD_MASK_TURN, turn);
+        }
+
+        public void addUser(short user) {
+            this.bitManipulate(HEAD_INDEX_USER, HEAD_MASK_USER, (byte)((user & 0xff00) >> 8));
+            this.bitManipulate(HEAD_INDEX_USER + 1, HEAD_MASK_USER, (byte)(user & 0x00ff));
+        }
+        
+        public void addLength(short length) {
+            this.bitManipulate(HEAD_INDEX_LENGTH, HEAD_MASK_LENGTH, (byte)((length & 0xff00) >> 8));
+            this.bitManipulate(HEAD_INDEX_LENGTH + 1, HEAD_MASK_LENGTH, (byte)(length & 0x00ff));
+        }
+
+        public int getType() {
+            return ((this.mData[HEAD_INDEX_TYPE] & HEAD_MASK_TYPE) >> 1);
+        }
+
+        public int getLength() {
+            return ((this.mData[HEAD_INDEX_LENGTH] << 8) | this.mData[HEAD_INDEX_LENGTH + 1]);
+        }
+
+        public byte[] getData() {
+            return this.mData;
+        }
+    }
+
+    // ----- Constants -----
+
+    // ----- Data -----
+    
+    // Data packet header
+    private Header mHeader;
+    
+    // Body of packet
+    private byte[] mBody;
+
+    // ----- Methods -----
+    
+    public DataPacket() {
+        this.mHeader = new Header();
+    }
+
+    // ----- Serialization -----
+
+    private void setByte(int index, byte data) {
+        this.mBody[index] = data;
+    }
+
+    // Sets up data with game state data
+    public void serializeData(Grid grid) {
+        // Add length for header info
+        this.mHeader.addType(Header.HEAD_TYPE_GAME);
+        this.mHeader.addLength((short)(Grid.GRID_SIZE * Grid.GRID_SIZE));
+        
+        // Copy header into packet
+        this.mBody = new byte[Header.HEADER_SIZE + this.mHeader.getLength()];
+        System.arraycopy(this.mHeader.getData(), 0, this.mBody, 0, Header.HEADER_SIZE);
+        
+        // Copy cell data into packet
+        var cells = grid.getCells();
+        for (int i = 0; i < Grid.GRID_SIZE; i++) {
+            for (int j = 0; j < Grid.GRID_SIZE; j++) {
+                int index = i * Grid.GRID_SIZE + j + Header.HEADER_SIZE;
+                this.setByte(index, cells[i][j].getCell());
+            }
+        }
+    }
+    
+    // Sets up packet with ping data
+    public void serializeData() {
+        this.mHeader.addType(Header.HEAD_TYPE_PING);
+        this.mHeader.addLength((short)0);
+
+        // Empty body, but not null
+        this.mBody = new byte[1];
+        this.setByte(0, (byte)0);
+    }
+
+    public byte[] getBuffer() {
+        return this.mBody;
+    }
+}
+
+/*
+
 public class DataPacket {
     final int gridLength = 10;
     class Header {
@@ -9,6 +145,7 @@ public class DataPacket {
         short userKey;
         // might not be needed??? java tcp seems to handle everything without needing a size
         int length;
+
         Header() {
             this.itemSentAndTurn = 0;
             this.userKey = 0;
@@ -18,6 +155,7 @@ public class DataPacket {
         public void setItemSent(int someFlag) {
             this.itemSentAndTurn = (byte) (this.itemSentAndTurn & 0b00011111);
         }
+
         public void setTurn(int someFlag) {
             this.itemSentAndTurn = (byte) (this.itemSentAndTurn & 0b11110001);
         }
@@ -25,6 +163,7 @@ public class DataPacket {
         public void setUserKey(short userKey) {
             this.userKey = userKey;
         }
+
         public void setLength(int length) {
             this.length = length;
         }
@@ -35,6 +174,7 @@ public class DataPacket {
         }
         
     }
+
     Header head;
     GridCell [][] boardMatrix;
     String buffer;
@@ -42,12 +182,15 @@ public class DataPacket {
         this.head = new Header();
         this.boardMatrix = new GridCell[gridLength][gridLength];
     }
+
     public DataPacket(String data) {
         // somehow parse the string data into usable data
     }
+
     public void setBoardMatrix(GridCell[][] matrix) {
         this.boardMatrix = matrix;
     }
+
     private void serializeData() {
         this.buffer = this.head.toString();
         for (int i = 0; i < gridLength; i++) {
@@ -56,9 +199,11 @@ public class DataPacket {
             }
         }
     }
-    public String getBuffer() {
+
+    public byte[] getBuffer() {
         serializeData();
         return this.buffer;
     }
 }
     
+*/
--- Server/src/main/java/com/belgianwaffles/battleshipserver/Main.java
+++ Server/src/main/java/com/belgianwaffles/battleshipserver/Main.java
@@ -1,92 +1,30 @@
 package com.belgianwaffles.battleshipserver;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
-    // main simply does not understand what im doing
-    // suppressing its warnings
-    @SuppressWarnings({ "unused", "null" })
+
     public static void main(String[] args) {
-        final int gridLength = 10;
-        System.out.println("Hello World!");
-        GridCell [][] boardMatrix = new GridCell[gridLength][gridLength];
-        DataPacket recievingP1Packet = null;
-        DataPacket recievingP2Packet = null;
-        DataPacket sendingPacketP1 = null;
-        DataPacket sendingPacketP2 = null;
-        ServerSocket serverP1Socket;
-        ServerSocket serverP2Socket;
-        Socket clientP1Socket;
-        Socket clientP2Socket;
-
-        while(true) {
-            try {
-                serverP1Socket = new ServerSocket(27000);
-                System.out.println("Server socket P1 created");
-                serverP2Socket = new ServerSocket(27001);
-                System.out.println("Server socket P2 created");
-
-                // accept client socket connection
-                clientP1Socket = serverP1Socket.accept();
-                System.out.println("Client P1 connected");
-
-                clientP2Socket = serverP2Socket.accept();
-                System.out.println("Client P2 connected");
-
-                // setup input and output streams
-                BufferedReader inP1 = new BufferedReader(new InputStreamReader(clientP1Socket.getInputStream()));
-                PrintWriter outP1 = new PrintWriter(clientP1Socket.getOutputStream(), true);
-
-                BufferedReader inP2 = new BufferedReader(new InputStreamReader(clientP2Socket.getInputStream()));
-                PrintWriter outP2 = new PrintWriter(clientP2Socket.getOutputStream(), true);
-
-                // Use this as an initial setup phase to figure out p1/p2 turn order and ship setup?
-
-
-                // get message from P1 client
-                String p1Message = inP1.readLine();
-                System.out.println("Client says: " + p1Message);
-                recievingP1Packet = new DataPacket(p1Message);
-
-                // get message from P2 client
-                String p2Message = inP2.readLine();
-                System.out.println("Client says: " + p2Message);
-                recievingP2Packet = new DataPacket(p2Message);
-
-                // do some stuff with sending packets to prep it for sending a response
-                // not sure if we've decided on any specific values for flags yet so
-                // im hesitant to do this myself
-                // ex:
-                // sendingPacketP1.head.setItemSent(someVal);
-                // sendingPacketP2.head.setItemSent(someVal);
-                // sendingPacketP1.head.setTurn(someVal);
-                // sendingPacketP1.setBoardMatrix(boardMatrix);
-                // etc
-
-
-                // send packet to client
-                outP1.println(sendingPacketP1.getBuffer());
-                outP2.println(sendingPacketP2.getBuffer());
-
-                // main gameplay loop(?)
-                boolean exitCondition = true;
-                while (exitCondition) {
-                    // play game
-
-                }
-
-                clientP1Socket.close();
-                serverP1Socket.close();
-                clientP2Socket.close();
-                serverP2Socket.close();
-            } catch (IOException ex) {
-                System.out.println("An error occured while communicating with client");
-            }
+        // Create server and check connections on separate thread
+        ConnectionManager connection;
+        try {
+            connection = new ConnectionManager(ConnectionManager.DEFAULT_PORT);
+            Thread connectThread = new Thread(connection);
+            connectThread.setDaemon(true);
+            connectThread.start();
+        } catch (IOException e) {
+            System.err.println("Could not create server socket");
+            return;
         }
+
+        // Allows server to properly close with input
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type anything to end server");
+        input.nextLine();
+
+        System.out.println("Exiting");
+        input.close();
+        connection.close();
     }
 }
