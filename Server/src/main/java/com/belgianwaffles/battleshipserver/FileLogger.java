0206f25cf0c20a471249cfa190d671a37e9e507c
Ian Blackmore <Iblackmore4899@conestogac.on.ca>
2025-03-16 22:29:56 -0400

    Added FileLogger tests

--- Server/src/main/java/com/belgianwaffles/battleshipserver/FileLogger.java
+++ Server/src/main/java/com/belgianwaffles/battleshipserver/FileLogger.java
@@ -17,6 +17,7 @@ public class FileLogger {
     private static final String LOG_GAME  = "game.log";
     private static final String LOG_ERROR = "error.log";
     private static final String LOG_PATH  = "Logs/";
+    private static final String TEST_PATH = LOG_PATH + "Tests/";
 
     private static final Logger logger = Logger.getLogger("FileLogger");
 
@@ -32,27 +33,44 @@ public class FileLogger {
     /**
      * Initializes the logger with all needed file paths, settings and formatting
      */
-    public static void initLogger() {
-
+    public static boolean initLogger() {
+        // prevents double initialization
+        if (pingLog != null && errorLog != null && gameLog != null) {
+            return true;
+        }
         try {
             // set false to get rid of append
             // setting pings to false since theres so many of them
             pingLog = new FileHandler(LOG_PATH+LOG_PING, false);
             gameLog = new FileHandler(LOG_PATH+LOG_GAME, true);
             errorLog = new FileHandler(LOG_PATH+LOG_ERROR, true);
+        } 
+        catch (IOException | SecurityException ex) {
+                
+            try {
+                System.err.println("Could not create file logger in standard log path, retrying.");
+                // attempt to cd into server to create logs
+                pingLog = new FileHandler("Server/"+LOG_PATH+LOG_PING, false);
+                gameLog = new FileHandler("Server/"+LOG_PATH+LOG_GAME, true);
+                errorLog = new FileHandler("Server/"+LOG_PATH+LOG_ERROR, true);
+            } 
+            catch (IOException | SecurityException ex1) {
+                System.err.println("Could not create file loggers");
+                return false;
+            }
             
-            SimpleFormatter formatter = new SimpleFormatter();
-            gameLog.setFormatter(formatter);
-            pingLog.setFormatter(formatter);
-            errorLog.setFormatter(formatter);
-            // this prevents logger from printing to stdout
-            // seeing every ping in stdout would get quite annoying
-            logger.setUseParentHandlers(false);
-
-        } catch (IOException | SecurityException ex) {
-            System.out.println("Error creating file logger.");
         }
-        
+
+        SimpleFormatter formatter = new SimpleFormatter();
+        gameLog.setFormatter(formatter);
+        pingLog.setFormatter(formatter);
+        errorLog.setFormatter(formatter);
+        // this prevents logger from printing to stdout
+        // seeing every ping in stdout would get quite annoying
+        logger.setUseParentHandlers(false);
+
+    
+        return true;
     }
 
     // Logging methods
@@ -61,11 +79,12 @@ public class FileLogger {
      * Logs a message being transmitted or received to the game log
      * @param message <code>String</code> Message to be written to the log
      */
-    public static void logMessage(String message) {
+    public static boolean logMessage(String message) {
         // logger is pain and this is how you have to manage multiple files
         logger.addHandler(gameLog);
         logger.log(Level.INFO, "{0}\n", message);
         logger.removeHandler(gameLog);
+        return true;
     }
 
     /**
@@ -75,23 +94,67 @@ public class FileLogger {
      * @param sourceMethod <code>String</code> Class the error came from
      * @param message <code>String</code> Message to be written to the log
      */
-    public static synchronized <T> void logError(Class <T> sourceClass, String sourceMethod, String message) {
+    public static synchronized <T> boolean logError(Class <T> sourceClass, String sourceMethod, String message) {
         // this is why logger got deprecated
         logger.addHandler(errorLog);
         logger.logp(Level.WARNING, sourceClass.getName(), sourceMethod, 
         "Error in class " + sourceClass.getName() + " in method " + sourceMethod + 
         "\nmessage: " + message + "\n");
         logger.removeHandler(errorLog);
+        return true;
     }
 
     /**
      * Logs a ping to the ping log
      * @param message <code>String</code> Ping to be written to the log
      */
-    public static void logPing(String message) {
+    public static boolean logPing(String message) {
         logger.addHandler(pingLog);
         logger.log(Level.INFO, "{0}\n", message);
         logger.removeHandler(pingLog);
+        return true;
     }
 
+
+    // TESTING VERSION
+    // just so our main log folder doesnt get flooded
+    public static boolean initLoggerTest() {
+        // prevents double initialization
+        if (pingLog != null && errorLog != null && gameLog != null) {
+            return true;
+        }
+        try {
+            // set false to get rid of append
+            // setting pings to false since theres so many of them
+            pingLog = new FileHandler(TEST_PATH+LOG_PING, false);
+            gameLog = new FileHandler(TEST_PATH+LOG_GAME, false);
+            errorLog = new FileHandler(TEST_PATH+LOG_ERROR, false);
+        } 
+        catch (IOException | SecurityException ex) {
+                
+            try {
+                System.err.println("Could not create file logger in standard log path, retrying.");
+                // attempt to cd into server to create logs
+                pingLog = new FileHandler("Server/"+TEST_PATH+LOG_PING, false);
+                gameLog = new FileHandler("Server/"+TEST_PATH+LOG_GAME, false);
+                errorLog = new FileHandler("Server/"+TEST_PATH+LOG_ERROR, false);
+            } 
+            catch (IOException | SecurityException ex1) {
+                System.err.println("Could not create file loggers");
+                return false;
+            }
+            
+        }
+
+        SimpleFormatter formatter = new SimpleFormatter();
+        gameLog.setFormatter(formatter);
+        pingLog.setFormatter(formatter);
+        errorLog.setFormatter(formatter);
+        // this prevents logger from printing to stdout
+        // seeing every ping in stdout would get quite annoying
+        logger.setUseParentHandlers(false);
+
+    
+        return true;
+    }
 }
