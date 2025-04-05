7c24e0dc1b9ce3f35960ef53144ce46bcde25c11
Andrew Abrantes <145807329+AndrewAConestoga@users.noreply.github.com>
2025-04-05 12:08:15 -0400

    added GameWindow Tests

--- Client/src/test/java/com/belgianwaffles/battleship/GameWindowTest.java
+++ Client/src/test/java/com/belgianwaffles/battleship/GameWindowTest.java
@@ -1,11 +1,36 @@
 package com.belgianwaffles.battleship;
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class GameWindowTest {
+
+    // since this class manages the UI, most tests have been done manually
+
+    @Test
+    public void setTurnCorrectly() {
+        GameWindow game = new GameWindow();
+
+        // check if turn
+        game.setTurn(true);
+        assertTrue(game.isTurn());
+
+        // check if not turn
+        game.setTurn(false);
+        assertTrue(!game.isTurn());
+    }
+
+    @Test 
+    public void startGameSetsGameStarted() {
+        GameWindow game = new GameWindow();
+
+        // check if game has not been started
+        assertTrue(!game.isGameStarted());
+
+        // start game and check if game has been started
+        game.startGame(new Grid());
+        assertTrue(game.isGameStarted());
+
+    }
 
 }
